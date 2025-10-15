591bb3a648dd3383e6348cc0e07ee512119a6815
bobby1072 <bobby.thompson1072@gmail.com>
2025-10-15 23:09:47 +0100

    Done

--- src/FishScraper.Core/FishScraper.Core.Api/Program.cs
+++ src/FishScraper.Core/FishScraper.Core.Api/Program.cs
@@ -1,20 +1,56 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text.Json;
+using BT.Common.Api.Helpers.Extensions;
+using BT.Common.Helpers;
 
+var localLogger = LoggingHelper.CreateLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    localLogger.LogInformation("Application is starting...");
+    
+    var builder = WebApplication.CreateBuilder(args);
+    builder.WebHost.ConfigureKestrel(options => options.AddServerHeader = false);
+
+    builder.Services.AddJsonLogging();
+
+    builder
+        .Services.AddControllers()
+        .AddJsonOptions(opts => { opts.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase; });
+    
+    builder.Services.AddResponseCompression();
+    builder.Services.AddHealthChecks();
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseRouting();
+
+    app.UseResponseCompression();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app
+        .UseCorrelationIdMiddleware();
+
+    app.MapControllers();
+
+    app
+        .UseHealthGetEndpoints();
+
+    await app.RunAsync();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    localLogger.LogCritical(ex, "Unhandled exception during application startup...");
+}
+finally
+{
+    localLogger.LogInformation("Application is exiting...");
+}
